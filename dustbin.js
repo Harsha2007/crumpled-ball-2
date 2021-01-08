7ee4378243216870b0a9d77a06b96902b0ae5b42
Harsha2007 <74820191+Harsha2007@users.noreply.github.com>
2021-01-08 17:47:47 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -1,28 +1,33 @@
-class DustBin {  
-    constructor( x, y, width, height,){
-        var options = {
-            'isStatic' : true
-        }
-        this.dustbinWidth=width; 
-        this.dustbinHeight=height;
-         this.wallThickness=20;
-        this.bodyb = Bodies.rectangle(x,y,  this.dustbinWidth, this.dustbinHeight,options );
-	    World.add(world, this.bodyb);
-	    this.bodyl= Bodies.rectangle((x-(width/2)), (y-(height*2)), height, width/2 ,options);
-	    World.add(world, this.bodyl);
-	    this.bodyr = Bodies.rectangle((x+(width/2)), (y-(height*2)), height, width/2 ,options);
-        World.add(world, this.bodyr);
-        this.bodyb.width = width
-        this.bodyb.height = height 
-        this.image=loadImage('dustbin.png');
-        
-   }
-   display(){
-    var posBottom=this.bodyb.position; 
-    var posLeft=this.bodyl.position; 
-    var posRight=this.bodyr.position;
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=213;
+		this.wallThickness=20;
+		
+		this.image=loadImage("dustbingreen.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		
 
-       
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
 
 			push()
 			translate(posLeft.x, posLeft.y);
@@ -54,8 +59,7 @@ class DustBin {
 			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
 			//rect(0,0,this.dustbinWidth, this.wallThickness);
 			pop()
+			
+	}
 
-}
-
-   
 }
--- sketch.js
+++ sketch.js
@@ -1,50 +1,70 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-var ground, gameState,engine, world,dustbin,paper;
+
 function setup() {
-  createCanvas(800, 400);
-  rectMode(CENTER);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
-  gameState = "start";
 
-  engine = Engine.create();
-  world = engine.world;
-  Engine.run(engine);
+	engine = Engine.create();
+	world = engine.world;
+	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	//Create a Ground
+	
 
-  dustbin = new DustBin(690, 390, 213, 130);
-  paper = new Paper(200,450,70);
-  ground = Bodies.rectangle(width / 2, 400, width, 10,
-  {
-    isStatic: true
-  });
-  World.add(world, ground);
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1600,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	Render.run(render);
+  
 }
+
 
 function draw() {
-  if (gameState === "start") {
-    background(230);
-    textSize(20);
-    fill("red");
-    text(" Press Up Arrow to Start, and Up to throw away the trash.", 50, 200)
-    if (keyCode === UP_ARROW) {
-      gameState = "play"
-    }
-  }
-  if (gameState === "play") {
-    rectMode(CENTER);
-    background(230);
-    dustbin.display();
-    paper.display();
+  rectMode(CENTER);
+  background(230);
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
 
-  }
+  
+  
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
 }
 
 
-function keyPressed(){
-  if (keyCode === UP_ARROW && gameState === "play") {
-    Matter.Body.applyForce(paper.body, paper.body.position, {x:130,y:-145});
-  }
-}
+
+
+
